920727856e38fe25024fc6ef239b5dd2d0ba3f9e
Darius Strasel <strasel.darius@gmail.com>
2017-05-20 19:41:01 -0400

    Add computation blocking example.

---
+++ utilities/_fibonacci.js
@@ -0,0 +1,15 @@
+/**
+ * Created by dariusstrasel on 5/20/17.
+ */
+
+var recursive = function (n) {
+    if(n <= 2) {
+        return 1;
+    } else {
+        return recursive(n - 1) + recursive(n - 2);
+    }
+};
+
+module.exports = {
+    fibonacci: recursive
+};
---
+++ utilities/computationalBlocking.js
@@ -0,0 +1,8 @@
+/**
+ * Created by dariusstrasel on 5/20/17.
+ */
+var fib = require('./_fibonacci');
+
+console.log(1);
+console.log(fib.fibonacci(42));
+console.log(2);
