38300aed11f8e50a07fd05167553d58028088635
Darius Strasel <strasel.darius@gmail.com>
2017-05-20 20:27:57 -0400

    Return 'index.html' on '/' route.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('port', 3000);
 app.get('/', function (req, res) {
     console.log("GET the homepage.");
     res.status(404);
-    res.send("Express yourself.");
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/json', function (req, res) {
