03821b1bb8775e311e25389a4a42e2e7e838dff9
Darius Strasel <strasel.darius@gmail.com>
2017-05-20 20:12:55 -0400

    Move node focused examples to seperate file.

---
+++ node_helloworld.js
@@ -0,0 +1,16 @@
+/**
+ * Created by dariusstrasel on 5/20/17.
+ */
+var talk = require('./talk');
+
+console.log("1. Start app");
+talk.intro();
+
+var holdOn = setTimeout(function () {
+    console.log("2. In the setTimeout().");
+}, 1000);
+
+talk.goodbye("World");
+talk.hello("Person");
+
+console.log("3. End app.");
