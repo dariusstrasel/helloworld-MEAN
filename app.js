1db41552c24126957ba1fd93e678256438270ce3
Darius Strasel <strasel.darius@gmail.com>
2017-05-20 20:18:20 -0400

    Add default '/' route.

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var express = require('express');
 var app = express();
 
 app.set('port', 3000);
+
+app.get('/', function (req, res) {
+    console.log("GET the homepage.");
+    res.status(404);
+    res.send("Express yourself.");
+});
+
 var server = app.listen(app.get('port'), function () {
     var port = server.address().port;
     console.log("Magic happens on port ", port);
