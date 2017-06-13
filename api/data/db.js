6cd9a05a68d2ff0cdb74ed81c6eea9a9c1ae38e6
Darius Strasel <strasel.darius@gmail.com>
2017-06-12 21:00:16 -0400

    Add mongoose client definition/ event listeners/ file system alerts.

---
+++ api/data/db.js
@@ -0,0 +1,46 @@
+/**
+ * Created by dariusstrasel on 5/21/17.
+ */
+var mongoose = require('mongoose');
+var dbURL = 'mongodb://localhost:27017/meanDatabase';
+
+mongoose.connect(dbURL);
+
+// Mongoose Event Listeners
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to:', dbURL);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected to:', dbURL);
+});
+
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection  error:', err);
+});
+
+// Linux/Unix File System Events
+
+process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+        console.log("Mongoose disconnected through app termination. (SIGINT)");
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function () {
+    mongoose.connection.close(function () {
+        console.log("Mongoose disconnected through app termination. (SIGTERM)");
+        process.exit(0);
+    });
+});
+
+process.once('SIGUSR2', function () {
+    mongoose.connection.close(function () {
+        console.log("Mongoose disconnected through app termination. (SIGUSR2)");
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+// Schemas and models
+require('./model.js');
