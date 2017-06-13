738adc978f10eef13b95d58e3a411def235ada04
Darius Strasel <strasel.darius@gmail.com>
2017-06-12 20:57:09 -0400

    Add data model for Mongoose.

--- api/data/model.js
+++ api/data/model.js
@@ -1,3 +1,24 @@
 /**
  * Created by dariusstrasel on 5/21/17.
  */
+var mongoose = require('mongoose');
+
+var dataSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    stars: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0
+    },
+    services: [String],
+    description: String,
+    photos: [String],
+    currency: String
+});
+
+// Model export to database
+mongoose.model('Hotel', dataSchema, 'hotels');
