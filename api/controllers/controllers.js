865e346101c04ebe3e56051142b0ec6b9dbcedeb
Darius Strasel <strasel.darius@gmail.com>
2017-06-13 23:47:20 -0400

    Add update and delete data methods.

--- api/controllers/controllers.js
+++ api/controllers/controllers.js
@@ -178,6 +178,81 @@ var addOneData = function (req, res) {
 
 };
 
+var updateOne = function (req, res) {
+    var hotelId = req.params.hotelId;
+    console.log("GET hotelId", hotelId);
+
+    Hotel
+        .findById(hotelId)
+        .select("-reviews -rooms")
+        .exec(function (err, doc) {
+            var response = {
+                status: 200,
+                message: doc
+            };
+            if (err) {
+                console.log("Error finding hotel");
+                response.status = 500;
+                response.message = err;
+            } else if (!doc) {
+                response.status = 404;
+                response.message = {
+                    "message": "Hotel ID not found"
+                };
+            }
+            if (response.status !== 200) {
+                res
+                .status(response.status)
+                .json(response.message);
+            } else {
+                doc.name = req.body.name;
+                doc.description = req.body.description;
+                doc.stars = parseInt(req.body.stars, 10);
+                doc.services = _splitArray(req.body.services);
+                doc.photos = _splitArray(req.body.photos);
+                doc.currency = req.body.currency;
+                doc.location = {
+                address: req.body.address,
+                coordinates: [
+                    parseFloat(req.body.lng),
+                    parseFloat(req.body.lat)
+                ]
+                };
+
+                doc.save(function (err, hotelUpdated) {
+                    if(err) {
+                        res
+                            .status(500)
+                            .json(err);
+                    } else {
+                        res
+                            .status(204)
+                            .json();
+                    }
+                })
+            }
+
+        });
+};
+
+var deleteOneData = function (req, res) {
+    var hotelId = req.params.hotelId;
+    Hotel
+        .findByIdAndRemove(hotelId)
+        .exec(function (err, hotel) {
+            if (err) {
+                res
+                    .status(404)
+                    .json(err);
+            } else {
+                console.log("Hotel deleted, id: " + hotelId);
+                res
+                    .status(204)
+                    .json();
+            }
+        });
+};
+
 var getFile = function (req, res) {
     console.log("GET the file.");
     res.status(200);
@@ -188,5 +263,7 @@ module.exports = {
     getAllData: getAllData,
     getOneData: getOneData,
     addOneData: addOneData,
+    updateOne: updateOne,
+    deleteOneData: deleteOneData,
     getFile: getFile
 };
