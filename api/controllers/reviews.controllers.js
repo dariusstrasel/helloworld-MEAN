9f9dbd56b9df0c06b39e029d161f71c92c60201c
Darius Strasel <strasel.darius@gmail.com>
2017-06-13 22:12:28 -0400

    Add review post method; add sub-doc method for inserting review into hotel.

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -92,7 +92,76 @@ var reviewsGetOne = function(req, res) {
         });
     };
 
+var _addReview = function (req, res, hotel) {
+
+    hotel.reviews.push({
+        name: req.body.name,
+        rating: parseInt(req.body.rating, 10),
+        review: req.body.review
+    });
+
+    hotel.save(function(err, hotelUpdated) {
+       if (err) {
+           res
+               .status(500)
+               .json(err);
+       } else {
+           res
+               .status(201)
+               .json(hotelUpdated.reviews[hotelUpdated.reviews.length - 1]);
+       }
+    });
+};
+
+var reviewsAddOne = function(req, res) {
+    var hotelId = req.params.hotelId;
+
+    console.log("GET hotelID", hotelId);
+    Hotel
+        .findById(hotelId)
+        .select('reviews')
+        .exec(function (err, doc) {
+            console.log("Returned " + doc);
+            var response = {
+                'status': 200,
+                'message': doc
+            };
+
+            if (!doc) {
+            response.status = 404;
+            response.message = {
+                "message": "Could not find hotel ID " + hotelId
+                }
+            } else {
+
+                if (err) {
+                    response.status = 500;
+                    response.message =
+                        {"message": "Could not find hotel for review lookup."};
+                }
+
+                if (!doc.reviews) {
+                    response.status = 200;
+                    response.message = {
+                        "message": "No reviews found."
+                    };
+                }
+            }
+
+            if (doc) {
+                _addReview(req, res, doc);
+            } else {
+                res
+                    .status(response.status)
+                    .json(response.message);
+            }
+
+        });
+
+};
+
 module.exports = {
     reviewsGetAll: reviewsGetAll,
-    reviewsGetOne: reviewsGetOne
+    reviewsGetOne: reviewsGetOne,
+    reviewsAddOne: reviewsAddOne
 };
