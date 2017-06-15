b9e2fadec689502e6944e1cae13e7c2653472270
Darius Strasel <strasel.darius@gmail.com>
2017-06-15 19:17:46 -0400

    Add comments + add node_modules static route.

--- app.js
+++ app.js
@@ -8,18 +8,25 @@ var path = require('path');
 var routes = require('./api/routes');
 var bodyParser = require('body-parser');
 
+// Define the webserver port
 app.set('port', 3000);
 
+// Middleware which prints every incoming request
 app.use(function (req, res, next) {
     console.log(req.method, req.url);
     next();
 });
 
+// Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'))
 
+// Enable parsing of posted forms
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Route defitions
 app.use('/api', routes);
+
 
 app.get('/', function (req, res) {
     console.log("GET the homepage.");
@@ -27,6 +34,8 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+
+// Evoke server and listen for requests
 var server = app.listen(app.get('port'), function () {
     var port = server.address().port;
     console.log("Magic happens on port ", port);
