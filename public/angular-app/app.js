9bbf11b00eae0b92f396607c68042fb4bdf00c10
Darius Strasel <strasel.darius@gmail.com>
2017-06-15 19:28:12 -0400

    Remove inline hotelcontroller reference; refactor templateURL to use new folder.

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -2,19 +2,13 @@
  * Created by dariusstrasel on 6/15/17.
  */
 angular.module('meanhotel', ['ngRoute'])
-.config(config)
-.controller('HotelsController', HotelsController);
+.config(config);
 
 function config($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'angular-app/hotels.html',
+            templateUrl: 'angular-app/hotel-list/hotels.html',
             controller: HotelsController,
             controllerAs: 'vm'
         });
-}
-
-function HotelsController() {
-    var vm = this;
-    vm.title = "MEAN Hotel App";
 }
