42db50bd007043756ed5b9bf9fb2eb0ad67a6562
Darius Strasel <strasel.darius@gmail.com>
2017-06-15 22:48:21 -0400

    Add hotel ID route

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -1,14 +1,18 @@
 /**
  * Created by dariusstrasel on 6/15/17.
  */
-angular.module('meanhotel', ['ngRoute'])
-.config(config);
+angular.module('meanhotel', ['ngRoute']).config(config);
 
 function config($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'angular-app/hotel-list/hotels.html',
             controller: HotelsController,
+            controllerAs: 'vm'
+        })
+        .when('/hotel/:id', {
+            templateUrl: 'angular-app/hotel-display/hotel.html',
+            controller: HotelController,
             controllerAs: 'vm'
         });
 }
