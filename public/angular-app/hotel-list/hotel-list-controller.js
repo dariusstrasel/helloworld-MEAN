74ede2058a3bd61a891e1328023d319fa49c8ab9
Darius Strasel <strasel.darius@gmail.com>
2017-06-15 22:51:34 -0400

    Refactor controller to use hotelDataFactory.

---
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dariusstrasel on 6/15/17.
+ */
+
+angular.module('meanhotel').controller('HotelsController', HotelsController);
+
+function HotelsController(hotelDataFactory) {
+    var vm = this;
+    vm.title = "MEAN Hotel App";
+    hotelDataFactory.hotelList().then(function (response) {
+        console.log(response);
+        vm.hotels = response;
+    })
+}
