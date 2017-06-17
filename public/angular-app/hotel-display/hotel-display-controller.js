1da6eae6c0bf85ca4a120523fe993f256660e212
Darius Strasel <strasel.darius@gmail.com>
2017-06-17 18:19:57 -0400

    Add addReview() method for posting data from form; add  paramter to controller.

--- public/angular-app/hotel-display/hotel-display-controller.js
+++ public/angular-app/hotel-display/hotel-display-controller.js
@@ -4,9 +4,10 @@
 
 angular.module("meanhotel").controller('HotelController', HotelController);
 
-function HotelController(hotelDataFactory, $routeParams) {
+function HotelController( $route, $routeParams,hotelDataFactory) {
     var vm = this;
     var id = $routeParams.id;
+    vm.isSubmitted = false;
     hotelDataFactory.hotelDisplay(id).then(function (response) {
         vm.hotel = response;
         vm.stars = _getStarRating(response.stars);
@@ -16,4 +17,21 @@ function HotelController(hotelDataFactory, $routeParams) {
         return new Array(stars);
     }
 
+    vm.addReview = function () {
+        var postData = {
+            name: vm.name,
+            rating: vm.rating,
+            review: vm.review
+        };
+        if (vm.reviewForm.$valid) {
+            hotelDataFactory.postReview(id, postData).then(function (response) {
+                console.log("response:", response);
+                $route.reload();
+            }).catch(function (error) {
+                console.log(error);
+            });
+        } else {
+            vm.isSubmitted = true;
+        }
+    };
 }
