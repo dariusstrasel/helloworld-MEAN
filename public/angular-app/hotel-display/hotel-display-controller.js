868365987b8beae690dd196181dffc0b4aa081bb
Darius Strasel <strasel.darius@gmail.com>
2017-06-17 16:34:08 -0400

    Add helper method to enable Stars Rating component

--- public/angular-app/hotel-display/hotel-display-controller.js
+++ public/angular-app/hotel-display/hotel-display-controller.js
@@ -9,5 +9,11 @@ function HotelController(hotelDataFactory, $routeParams) {
     var id = $routeParams.id;
     hotelDataFactory.hotelDisplay(id).then(function (response) {
         vm.hotel = response;
-    })
+        vm.stars = _getStarRating(response.stars);
+    });
+
+    function _getStarRating(stars) {
+        return new Array(stars);
+    }
+
 }
