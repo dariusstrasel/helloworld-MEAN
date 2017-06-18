a456e0b360136f4a8f56e983d830c6f447c16130
Darius Strasel <strasel.darius@gmail.com>
2017-06-18 17:14:19 -0400

    Add register route to AngularJS frontend.

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -14,5 +14,10 @@ function config($routeProvider) {
             templateUrl: 'angular-app/hotel-display/hotel.html',
             controller: HotelController,
             controllerAs: 'vm'
+        })
+        .when('/register', {
+            templateUrl: 'angular-app/register/register.html',
+            controller: RegisterController,
+            controllerAs: 'vm'
         });
 }
