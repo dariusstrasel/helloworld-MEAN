886227b6efdaa5df0622408e9704952386a811d3
Darius Strasel <strasel.darius@gmail.com>
2017-06-18 22:19:56 -0400

    Add auth-factory for checking login status on front-end.

---
+++ public/angular-app/auth/auth-factory.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dariusstrasel on 6/18/17.
+ */
+angular.module('meanhotel').factory('AuthFactory', AuthFactory);
+
+function AuthFactory() {
+    return {
+        auth: auth
+    };
+
+    var auth = {
+        isLoggedIn: false
+    };
+}
