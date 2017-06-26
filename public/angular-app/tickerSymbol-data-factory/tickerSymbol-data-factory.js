e4f3b19430aac2b704572ccaf93df2f34612f4b0
Darius Strasel <strasel.darius@gmail.com>
2017-06-25 20:51:33 -0400

    Add googleFinance controller for live stock data.

--- public/angular-app/tickerSymbol-data-factory/tickerSymbol-data-factory.js
+++ public/angular-app/tickerSymbol-data-factory/tickerSymbol-data-factory.js
@@ -7,7 +7,9 @@ function tickerSymbolDataFactory($http) {
 
     return {
         tickerSymbolList: tickerSymbolList,
-        tickerSymbolDisplay: tickerSymbolDisplay
+        tickerSymbolDisplay: tickerSymbolDisplay,
+        tickerSymbolDisplay_live: tickerSymbolDisplay_live,
+        tickerSymbolNews: tickerSymbolNews
     };
 
     function tickerSymbolList() {
@@ -16,8 +18,20 @@ function tickerSymbolDataFactory($http) {
             .catch(failed);
     }
 
+    function tickerSymbolDisplay_live(symbol) {
+        return $http.get(`https://finance.google.com/finance/info?client=ig&q=NASDAQ:${symbol}`)
+            .then(liveData)
+            .catch(failed);
+    }
+
+    function tickerSymbolNews(symbol) {
+        return $http.get(`http://webhose.io/filterWebContent?token=0db9b041-7e05-428f-8d8d-2e4c27ff19e5&format=json&ts=1498087100710&sort=crawled&q=a${symbol}`)
+            .then(liveData)
+            .catch(failed);
+    }
+
     function tickerSymbolDisplay(symbol) {
-        return $http.get('/api/tickerSymbols/' + symbol)
+      return $http.get('/api/tickerSymbols/' + symbol)
             .then(complete)
             .catch(failed);
     }
@@ -25,7 +39,33 @@ function tickerSymbolDataFactory($http) {
   // function postReview(id, review) {
   //   return $http.post('/api/hotels/' + id + '/reviews', review).then(complete).catch(failed);
   // }
-    
+
+
+
+    function processGoogleFinance(googleResponse) {
+        console.log("response: ", googleResponse);
+        stockData = JSON.parse(googleResponse.data.slice(3, googleResponse.data.length))[0];
+        console.log(stockData);
+        var stockDataReKeyed = function() {
+            return {
+                change: stockData['c'],
+                index: stockData['e'],
+                lastTradePrice: stockData['l'],
+                lastTradeDateTime: stockData['lt_dts'],
+                dividend: stockData['div'],
+                yield: stockData['yld'],
+                lastTradeSize: stockData['s'],
+                previousClosePrice: stockData['pcls_fix']
+            }
+        };
+        console.log("processed: ", stockDataReKeyed());
+        return stockDataReKeyed();
+    }
+
+    function liveData(response) {
+        return processGoogleFinance(response)
+    }
+
     function complete(response) {
         return response.data
     }
